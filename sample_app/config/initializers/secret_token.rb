e318ab71b4b4f7f863710d8bccdfe3d94d0ebfe3
Soshi Katsuta <soshi.katsuta@gmail.com>
2017-03-04 16:13:18 +0900

    create sample_app
    Signed-off-by: Soshi Katsuta <soshi.katsuta@gmail.com>

---
+++ sample_app/config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+SampleApp::Application.config.secret_key_base = 'bdf3c82d68526a165fc55f1e99349fe8db3cb2a4eabd19b2252e7352b6761cd963e82478d6a26a3c7d5e9b1de4b7e0afd2767f6c8635b6ca6ad2f8fbb9f8c588'
