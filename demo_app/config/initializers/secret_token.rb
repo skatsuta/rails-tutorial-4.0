d66b35a0215a2e9cae4beee3dbde13dd8ae6e7fe
Soshi Katsuta <soshi.katsuta@gmail.com>
2017-03-04 14:11:56 +0900

    create demo_app
    Signed-off-by: Soshi Katsuta <soshi.katsuta@gmail.com>

---
+++ demo_app/config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+DemoApp::Application.config.secret_key_base = 'd5487c2b2902dbe68138385cf1e5b9f34ac9303fbafe7a83c6d65f272d5e08cf094bd2c3608685557120aa626238ed019aa32c5515482ba5adffd604cc9c53ed'
