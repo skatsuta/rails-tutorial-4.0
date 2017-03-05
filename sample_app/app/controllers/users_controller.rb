544b7d7e045ef06ae6eca122b536d4a131989061
Soshi Katsuta <soshi.katsuta@gmail.com>
2017-03-05 22:07:43 +0900

    add a controller for microposts
    Signed-off-by: Soshi Katsuta <soshi.katsuta@gmail.com>

--- sample_app/app/controllers/users_controller.rb
+++ sample_app/app/controllers/users_controller.rb
@@ -53,12 +53,6 @@ class UsersController < ApplicationController
 
   # Before actions
 
-  def signed_in_user
-    return if signed_in?
-    store_location
-    redirect_to signin_url, notice: "Please sign in."
-  end
-
   def correct_user
     @user = User.find(params[:id])
     redirect_to(root_path) unless current_user?(@user)
