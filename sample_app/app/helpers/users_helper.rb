4d82199f7a0f1926d356ae1ae07131c0c5e258e3
Soshi Katsuta <soshi.katsuta@gmail.com>
2017-03-05 13:54:57 +0900

    add signin functionality
    Signed-off-by: Soshi Katsuta <soshi.katsuta@gmail.com>

--- sample_app/app/helpers/users_helper.rb
+++ sample_app/app/helpers/users_helper.rb
@@ -1,6 +1,6 @@
 module UsersHelper
   # Return a given user's Gravatar.
-  def gravatar_for(user, options = { size: 50 })
+  def gravatar_for(user, options = { size: 80 })
     gravatar_id = Digest::MD5::hexdigest(user.email.downcase)
     size = options[:size]
     gravatar_url = "https://secure.gravatar.com/avatar/#{gravatar_id}?s=#{size}"
--- sample_app/app/models/user.rb
+++ sample_app/app/models/user.rb
@@ -1,4 +1,5 @@
 class User < ActiveRecord::Base
+  before_create :create_remember_token
   before_save { email.downcase! }
 
   validates :name, presence: true, length: { maximum: 50 }
@@ -11,4 +12,18 @@ class User < ActiveRecord::Base
 
   has_secure_password
   validates :password, length: { minimum: 6 }
+
+  def self.new_remember_token
+    SecureRandom.urlsafe_base64
+  end
+
+  def self.encrypt(token)
+    Digest::SHA1.hexdigest(token.to_s)
+  end
+
+  private
+
+  def create_remember_token
+    self.remember_token = User.encrypt(User.new_remember_token)
+  end
 end
