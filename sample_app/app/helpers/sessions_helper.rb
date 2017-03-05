b663df655b4262f221072fe9542047590bef96af
Soshi Katsuta <soshi.katsuta@gmail.com>
2017-03-05 14:03:09 +0900

    add signout functionality
    Signed-off-by: Soshi Katsuta <soshi.katsuta@gmail.com>

--- sample_app/app/helpers/sessions_helper.rb
+++ sample_app/app/helpers/sessions_helper.rb
@@ -1,4 +1,13 @@
 module SessionsHelper
+  def current_user=(user)
+    @current_user = user
+  end
+
+  def current_user
+    remember_token = User.encrypt(cookies[:remember_token])
+    @current_user ||= User.find_by(remember_token: remember_token)
+  end
+
   def sign_in(user)
     remember_token = User.new_remember_token
     cookies.permanent[:remember_token] = remember_token
@@ -10,12 +19,8 @@ module SessionsHelper
     !current_user.nil?
   end
 
-  def current_user=(user)
-    @current_user = user
-  end
-
-  def current_user
-    remember_token = User.encrypt(cookies[:remember_token])
-    @current_user ||= User.find_by(remember_token: remember_token)
+  def sign_out
+    self.current_user = nil
+    cookies.delete(:remember_token)
   end
 end
