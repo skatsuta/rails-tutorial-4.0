86d8af00a83ba68095df3720fd00e46b8902083a
Soshi Katsuta <soshi.katsuta@gmail.com>
2017-03-05 17:29:56 +0900

    add users index page
    Signed-off-by: Soshi Katsuta <soshi.katsuta@gmail.com>

--- sample_app/spec/requests/authentication_pages_spec.rb
+++ sample_app/spec/requests/authentication_pages_spec.rb
@@ -28,9 +28,10 @@ describe "Authentication" do
 
     describe "with valid information" do
       let(:user) { FactoryGirl.create(:user) }
-      before { sign_in(user) }
+      before { sign_in user }
 
       it { should have_title(user.name) }
+      it { should have_link("Users", href: users_path) }
       it { should have_link("Profile", href: user_path(user)) }
       it { should have_link("Settings", href: edit_user_path(user)) }
       it { should have_link("Sign out", href: signout_path) }
@@ -74,6 +75,12 @@ describe "Authentication" do
           before { patch user_path(user) }
 
           specify { expect(response).to redirect_to(signin_path) }
+        end
+
+        describe "visiting the user index" do
+          before { visit users_path }
+
+          it { should have_title("Sign in")}
         end
       end
     end
